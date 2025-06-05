2fcce020f60831f244ea2cd22a61a8412a70aa9e
yuyuyu07 <575882583@qq.com>
2025-06-05 11:21:33 +0800

    首次上传
    部署角色扮演

---
+++ openai_role_playing.py
@@ -0,0 +1,30 @@
+import os  # 操作系统交互接口的库
+from openai import OpenAI  # 从openai库里导入OpenAI类
+
+# --- 创建函数 ---
+def get_gemini_response(api_key,messages_history: list):
+    # 创建OpenAI实例
+    client = OpenAI(
+        api_key=api_key,  # 使用传入的API密钥
+        base_url="https://generativelanguage.googleapis.com/v1beta/openai/"  # 使用传入的API地址
+    )
+
+    # 调用.chat.completions.create()方法
+    response = client.chat.completions.create(
+        model="gemini-2.0-flash",  # 使用传入的模型名称
+        messages=messages_history,  # 使用传入的聊天列表
+        temperature=0.8,  # 温度参数，控制生成文本的随机性
+    )
+
+    # 提取 生成文本内容
+    AI助手 = response.choices[0].message.role  # 响应对象.选项数组[0].消息.assistant
+    生成内容 = response.choices[0].message.content  # 响应对象.选项数组[0].消息.内容
+
+    return {"role": AI助手, "content": 生成内容}  # 返回AI的回复
+
+
+
+# 测试
+# messages_history = {"role":"user","content": "你好"}
+# AI聊天字典 = get_gemini_response("AIzaSyD_gl3lcKnfue6BEVJ_9uLUJQtAlEWcYS8",messages_history)
+# print(AI聊天字典)
